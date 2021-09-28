b40730876d5076438e6e08ec694a4b11e8a2ed8d
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-28 14:37:45 -0400

    added questions

---
+++ joins-solution.sql
@@ -0,0 +1,22 @@
+-- Question 1: Get all customers and their addresses.
+
+
+-- Question 2: Get all orders and their line items (orders, quantity and product).
+
+
+-- Question 3: Which warehouses have cheetos?
+
+
+-- Question 4: Which warehouses have diet pepsi?
+
+
+-- Question 5: Get the number of orders for each customer
+
+
+-- Question 6: How many customers do we have?
+
+
+-- Question 7: How many products do we carry?
+
+
+-- Question 8: What is the total available on-hand quantity of diet pepsi?
